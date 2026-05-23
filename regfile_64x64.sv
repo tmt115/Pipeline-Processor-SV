ed4d2e24c08efe92bc4cac3ede53b365f572fa02
Thomas Tedeschi <miketommy2006@gmail.com>
2026-05-22 22:50:53 -0400

    Register files initial upload (regfile not finished)

---
+++ regfile_64x64.sv
@@ -0,0 +1,11 @@
+module regfile_64x64 (
+    input  logic clk;
+    input  logic rst;
+
+    input  logic [5:0]  raddr0;
+    input  logic [5:0]  raddr1;
+    output logic [63:0] rdata0;
+    output logic [63:0] rdata1;
+
+    
+);
---
+++ register_64b.sv
@@ -0,0 +1,18 @@
+module register_64b (
+    input  logic [63:0] d;
+    input  logic        rst;
+    input  logic        clk;
+    output logic [63:0] q;
+);
+
+always_ff @(posedge clk) begin
+
+    if (rst) 
+        q <= 64'b0;
+    else
+        q <= d;
+
+end
+
+endmodule
+
