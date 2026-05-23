3456620f2f7f9fe48c980ef56f3141f4fcd9f629
Thomas Tedeschi <miketommy2006@gmail.com>
2026-05-22 23:08:59 -0400

    Regfile completed

--- regfile_64x64.sv
+++ regfile_64x64.sv
@@ -7,5 +7,35 @@ module regfile_64x64 (
     output logic [63:0] rdata0;
     output logic [63:0] rdata1;
 
-    
+    input  logic        w_en;
+    input  logic [5:0]  waddr;
+    input  logic [63:0] wdata;
 );
+
+logic [63:0] file[64];
+
+always_ff @(posedge clk) begin
+
+
+    if (res) begin
+        for (int x = 0; x < 64; x++) begin
+            file[x] <= 0;
+        end
+    end
+    if (waddr == 0) begin
+        file[waddr] <= 64'b0;
+    end
+    else begin
+        file[waddr] <= wdata;
+
+end
+
+always_comb begin
+    
+    rdata0 = file[raddr0];
+    rdata1 = file[raddr1];
+
+end
+
+endmodule
+
